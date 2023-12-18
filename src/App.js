5c6f9738ba329fee5e29b48bca71fbaf9239de27
Bamideleadekanye@gmail.com <bamideleadekanye@gmail.com>
2023-12-19 00:56:52 +0100

    rating select completed without delete animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import {v4 as uuidv4} from 'uuid'
 import {useState} from 'react'
 import Header from "./components/Header"
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
-//import FeedbackItem from './components/FeedbackItem'
+import FeedbackForm from './components/FeedbackForm'
+//import FeedbackItem from do//
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback,...feedback])
+
+    }
+
     const deleteFeedback = (id) => {
         if(window.confirm('Are you sure you want to delete?')) {
         setFeedback(feedback.filter((item) => item.id !==id))}
@@ -15,9 +24,10 @@ function App() {
     <>
         <Header />
         <div className="container">
-            <FeedbackStats feedback={feedback}/>
-            <FeedbackList feedback = {feedback} 
-            handleDelete={deleteFeedback}/>
+        <FeedbackForm handleAdd={addFeedback}/>
+        <FeedbackStats feedback={feedback}/>
+        <FeedbackList feedback = {feedback} 
+        handleDelete={deleteFeedback}/>
         </div>
     </>
  )
